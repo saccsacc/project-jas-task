cb3d723bf20f076a290a2dd72b78ef420940b974
saccsacc <sacchkkan@gmail.com>
2024-07-14 15:04:45 +0700

    create task table

---
+++ database/migrations/2024_07_13_023151_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',150);
+            $table->text('description');
+            $table->enum('status',['pending', 'in-progress','completed']);
+            $table->date('due_date',1)->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->index('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
